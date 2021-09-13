96811b36a1decc221db7fff37d617de735dfcdda
rajivbansal2981 <rajivbansal2981@DESKTOP-M89N8PJ>
2021-09-13 22:22:35 +0530

    JaiMataKi

---
+++ best-java-programs/src/main/java/com/jmk/codingtest/CountWordOccurrence.java
@@ -0,0 +1,27 @@
+package com.jmk.codingtest;
+
+public class CountWordOccurrence {
+
+	public static int countOccurrences(String str, String word) {
+		// split the string by spaces in a
+		String a[] = str.split(" ");
+
+		// search for pattern in a
+		int count = 0;
+		for (int i = 0; i < a.length; i++) {
+			// if match found increase count
+			if (word.equals(a[i]))
+				count++;
+		}
+
+		return count;
+	}
+
+	// Driver code
+	public static void main(String args[]) {
+		String str = "GeeksforGeeks A computer science portal for geeks ";
+		String word = "portal";
+		System.out.println(countOccurrences(str, word));
+	}
+
+}
